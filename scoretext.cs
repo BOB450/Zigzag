02123685b6725fd80671a2ae64d8c2f64ba0a514
BOB450 <bwvoskuil@gmail.com>
2021-08-28 15:25:00 -0600

    Add files via upload

---
+++ scoretext.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoretext : MonoBehaviour
+{
+    public Text scoretxt;
+    public int num;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoretxt.text = "0";
+        InvokeRepeating("addscore", 2.0f, 5.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void addscore() 
+    {
+        
+        num++;
+
+        string stringmy = num.ToString();
+
+        scoretxt.text = stringmy;
+    }
+}
